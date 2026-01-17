6af1db66386c7a45389e68afda59bdd3996a7a03
Aukarapol <jchai1023@gmail.com>
2026-01-17 21:56:11 +0700

    addd

---
+++ orderproduct.js
@@ -0,0 +1,21 @@
+// orderproduct.js
+const products = {
+    "P0001": {
+        name: "Tapo C530WS",
+        description: "กล้อง Wi-Fi ภายนอกอัจฉริยะ สั่งหมุน-ก้มเงยได้",
+        price: "1,190",
+        image: "productimage/P0001/Tapo_C530WS01.jpg"
+    },
+    "P0002": {
+        name: "Tapo C425 V1.2",
+        description: "Smart Wire-Free Security Camera",
+        price: "1,290",
+        image: "productimage/P0002/Tapo C425_01.jpg"
+    },
+    "P0003": {
+        name: "Tapo C645D KIT",
+        description: "Solar-Powered Dual-Lens Pan",
+        price: "1,700",
+        image: "productimage/P0003/Tapo_C645D_KIT01.jpg"
+    }
+};
