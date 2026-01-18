20fdad6068eef5a90bda87d1d280e79102dcde66
Aukarapol <jchai1023@gmail.com>
2026-01-18 14:00:31 +0700

    Update orderproduct.js

--- orderproduct.js
+++ orderproduct.js
@@ -15,7 +15,61 @@ const products = {
     "P0003": {
         name: "Tapo C645D KIT",
         description: "Solar-Powered Dual-Lens Pan",
-        price: "1,700",
+        price: "1,390",
         image: "productimage/P0003/Tapo_C645D_KIT01.jpg"
+    },
+    "P0004": {
+        name: "ซิมคงกระพัน 12 เดือน",
+        description: "ซิมคงกระพัน เน็ตและโทรพร้อมใช้",
+        price: "1,890",
+        image: "productimage/P0004/dtac_12_1.jpg"
+    },
+    "P0005": {
+        name: "ซิมคงกระพัน 6 เดือน",
+        description: "ซิมคงกระพัน เน็ตและโทรพร้อมใช้",
+        price: "1,390",
+        image: "productimage/P0005/dtac_6_1.jpg"
+    },
+    "P0006": {
+        name: "ซิมคงกระพัน 3 เดือน",
+        description: "ซิมคงกระพัน เน็ตและโทรพร้อมใช้",
+        price: "1,190",
+        image: "productimage/P0007/s25ultra_01.jpg"
+    },
+    "P0007": {
+        name: "Galaxy S25 Ultra",
+        description: "AI ProVisual Engine ที่ล้ำหน้าที่สุด",
+        price: "28,790",
+        image: "productimage/P0007/s25ultra_01.jpg"
+    },
+    "P0008": {
+        name: "Galaxy A36 5G",
+        description: "กล้องที่ดีกว่าเดิมเพื่อความสร้างสรรค์ที่มากกว่า",
+        price: "8,900",
+        image: "productimage/P0008/a36_01.jpg"
+    },
+    "P0009": {
+        name: "OPPO Reno 15",
+        description: "Solar-Powered Dual-Lens Pan",
+        price: "13,730",
+        image: "productimage/P0009/oppo_01.png"
+    },
+    "P0010": {
+        name: "Samsung S25 FE",
+        description: "เพรียวบางกว่า เบากว่า และพร้อมที่จะสร้างความประทับใจ",
+        price: "18,900",
+        image: "productimage/P0010/s25fe_02.jpg"
+    },
+    "P0011": {
+        name: "ซิมเทพทรู",
+        description: "ซิมเทพทรู เน็ตและโทรพร้อมใช้",
+        price: "1,790",
+        image: "productimage/P0011/true_01.jpeg"
+    },
+    "P0012": {
+        name: "Tapo C545D",
+        description: "Wide-Angle & Telephoto Lens",
+        price: "1,890",
+        image: "productimage/P0012/Tapo_C545D_01.jpg"
     }
 };
